4b1c263438fb23880e021a2673f79af38b7d72d5
adarsh-sharma6218 <adarshsharma6218@gmail.com>
2022-01-05 13:55:37 +0530

    feat: Add styling to the scroll bar of the library + Add more songs to the application

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Player from "./components/Player";
 import Song from "./components/Song";
 import "./styles/app.scss";
 import data from "./util";
-import LibrarySong from "./components/LibrarySong";
 import Library from "./components/Library";
 function App() {
   const [songs, setSongs] = useState(data());
